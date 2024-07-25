d13b740c6ffe5b0aa232afabe1443f70cf30d047
Adi Yogta Putra <adiyogta@gmail.com>
2024-07-26 02:15:52 +0700

    yaudah lah ya

--- src/app/musik/musik.component.ts
+++ src/app/musik/musik.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-musik',
@@ -6,46 +7,77 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   templateUrl: './musik.component.html',
   styleUrls: ['./musik.component.css']
 })
-export class MusikComponent implements OnInit, AfterViewInit {
-  audioPlayer!: HTMLAudioElement;
+export class MusikComponent implements OnInit, AfterViewInit, OnDestroy {
+  audioPlayer?: HTMLAudioElement;
   isPlaying: boolean = false;
   audioSource = '/assets/ph.mp3'; // Ganti dengan path file audio Anda
+  private visibilitySubscription?: Subscription;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.audioPlayer = new Audio();
-    this.audioPlayer.src = this.audioSource;
+    if (typeof Audio !== 'undefined') {
+      this.audioPlayer = new Audio(this.audioSource);
+      this.audioPlayer.loop = true;  // Membuat audio berulang
 
-    const shouldPlayMusicStr = localStorage.getItem('shouldPlayMusic');
-    this.isPlaying = shouldPlayMusicStr === 'true';
+      const shouldPlayMusicStr = localStorage.getItem('shouldPlayMusic');
+      this.isPlaying = shouldPlayMusicStr === 'true';
 
-    if (this.isPlaying) {
-      this.playMusic();
+      if (this.isPlaying) {
+        this.playMusic();
+      }
+
+      // Menangani perubahan visibilitas halaman
+      if (typeof document !== 'undefined') {
+        this.visibilitySubscription = fromEvent(document, 'visibilitychange').subscribe(() => {
+          if (document.hidden) {
+            this.pauseMusic();
+          } else if (this.isPlaying) {
+            this.playMusic();
+          }
+        });
+      }
     }
   }
 
   ngAfterViewInit(): void {
-    this.audioPlayer.autoplay = true;
-    this.audioPlayer.onended = () => {
-      this.isPlaying = false;
-    };
+    if (this.audioPlayer) {
+      this.audioPlayer.onended = () => {
+        if (this.isPlaying) {
+          this.playMusic();
+        }
+      };
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.pauseMusic();
+    if (this.audioPlayer) {
+      this.audioPlayer.src = '';
+    }
+    this.visibilitySubscription?.unsubscribe();
   }
 
   togglePlayPause() {
-    if (this.isPlaying) {
-      this.pauseMusic();
-    } else {
-      this.playMusic();
-    }
     this.isPlaying = !this.isPlaying;
+    localStorage.setItem('shouldPlayMusic', this.isPlaying.toString());
+    
+    if (this.isPlaying) {
+      this.playMusic();
+    } else {
+      this.pauseMusic();
+    }
   }
 
   playMusic() {
-    this.audioPlayer.play();
+    if (this.audioPlayer) {
+      this.audioPlayer.play().catch(error => console.error('Error playing audio:', error));
+    }
   }
 
   pauseMusic() {
-    this.audioPlayer.pause();
+    if (this.audioPlayer) {
+      this.audioPlayer.pause();
+    }
   }
 }
